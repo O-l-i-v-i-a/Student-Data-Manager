a3f0e8131c83c2649966228303382cf0b1617fd5
Olivia Mary <oliviamary.27csb@licet.ac.in>
2025-10-21 21:32:21 +0530

    Added better styling

--- students/forms.py
+++ students/forms.py
@@ -5,3 +5,15 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            css_class = 'form-control'
+            if isinstance(field.widget, forms.Select):
+                field.widget.attrs['class'] = css_class
+            elif isinstance(field.widget, forms.Textarea):
+                field.widget.attrs['class'] = css_class
+            else:
+                field.widget.attrs['class'] = css_class
+
--- students/views.py
+++ students/views.py
@@ -10,7 +10,7 @@ def student_list(request):
     students = Student.objects.all()
 
     # Get filter inputs from GET parameters
-    name_query = request.GET.get('q', '')  # existing search by name
+    name_query = request.GET.get('q', '')
     caste_query = request.GET.get('caste', '')
     religion_query = request.GET.get('religion', '')
     course_query = request.GET.get('course', '')
@@ -22,17 +22,23 @@ def student_list(request):
     if name_query:
         students = students.filter(name__icontains=name_query)
     if caste_query:
-        students = students.filter(caste__icontains=caste_query)
+        students = students.filter(caste=caste_query)
     if religion_query:
-        students = students.filter(religion__icontains=religion_query)
+        students = students.filter(religion=religion_query)
     if course_query:
-        students = students.filter(course__icontains=course_query)
+        students = students.filter(course=course_query)
     if year_query:
         students = students.filter(year_of_study=year_query)
     if min_income:
         students = students.filter(annual_income__gte=min_income)
     if max_income:
         students = students.filter(annual_income__lte=max_income)
+
+    # Dropdown options (distinct values) - order and exclude blanks
+    caste_options = Student.objects.order_by('caste').values_list('caste', flat=True).distinct()
+    religion_options = Student.objects.order_by('religion').values_list('religion', flat=True).distinct()
+    course_options = Student.objects.order_by('course').values_list('course', flat=True).distinct()
+    year_options = Student.objects.order_by('year_of_study').values_list('year_of_study', flat=True).distinct()
 
     context = {
         'students': students,
@@ -43,6 +49,10 @@ def student_list(request):
         'year_query': year_query,
         'min_income': min_income,
         'max_income': max_income,
+        'caste_options': [c for c in caste_options if c],
+        'religion_options': [r for r in religion_options if r],
+        'course_options': [c for c in course_options if c],
+        'year_options': [y for y in year_options if y],
     }
     return render(request, 'students/student_list.html', context)
 
