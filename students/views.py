81cbc29c0c51b9d2631891e441419244670982ee
Olivia Mary <oliviamary.27csb@licet.ac.in>
2025-10-21 20:36:54 +0530

    Added Filter option

--- students/views.py
+++ students/views.py
@@ -7,12 +7,44 @@ from .forms import StudentForm  # We'll create this next
 
 # List all students
 def student_list(request):
-    query = request.GET.get('q')
-    if query:
-        students = Student.objects.filter(name__icontains=query)
-    else:
-        students = Student.objects.all()
-    return render(request, 'students/student_list.html', {'students': students})
+    students = Student.objects.all()
+
+    # Get filter inputs from GET parameters
+    name_query = request.GET.get('q', '')  # existing search by name
+    caste_query = request.GET.get('caste', '')
+    religion_query = request.GET.get('religion', '')
+    course_query = request.GET.get('course', '')
+    year_query = request.GET.get('year_of_study', '')
+    min_income = request.GET.get('min_income', '')
+    max_income = request.GET.get('max_income', '')
+
+    # Apply filters
+    if name_query:
+        students = students.filter(name__icontains=name_query)
+    if caste_query:
+        students = students.filter(caste__icontains=caste_query)
+    if religion_query:
+        students = students.filter(religion__icontains=religion_query)
+    if course_query:
+        students = students.filter(course__icontains=course_query)
+    if year_query:
+        students = students.filter(year_of_study=year_query)
+    if min_income:
+        students = students.filter(annual_income__gte=min_income)
+    if max_income:
+        students = students.filter(annual_income__lte=max_income)
+
+    context = {
+        'students': students,
+        'name_query': name_query,
+        'caste_query': caste_query,
+        'religion_query': religion_query,
+        'course_query': course_query,
+        'year_query': year_query,
+        'min_income': min_income,
+        'max_income': max_income,
+    }
+    return render(request, 'students/student_list.html', context)
 
 # Create student
 def student_create(request):
@@ -44,3 +76,7 @@ def student_delete(request, pk):
         student.delete()
         return redirect('student_list')
     return render(request, 'students/student_confirm_delete.html', {'student': student})
+
+
+
+
